6b69005ca46330c0f13659f4458c6f61090f8833
김윤미 <yoonmihy@naver.com>
2023-03-28 23:29:36 +0900

    주석 추가

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -23,27 +23,30 @@ function App() {
       });
   };
 
+  //방 만들기
   const createRoom = () => {
-    // const params = new URLSearchParams();
-    // params.append("user1", "user1");
-    // params.append("user2", "user2");
-    // axios.post('http://localhost:8080/chat/room', params)
-    //   .then(response => {
-    //     alert(`${response.data} 방 개설에 성공하였습니다.`);
-    //   })
-    //   .catch(response => {
-    //     alert("채팅방 개설에 실패하였습니다.");
-    //   });
+    /*
+      소켓 연결
+      /chat으로 가는 요청에 대해 소켓 연결 설정
+    */
     const ws = new SockJS('http://localhost:8080/chat');
     const stompClient = Stomp.over(ws);
-    // const set = new Set([1, 2]); //user id 목록
+
+    /*
+      /chat/rooms를 구독하고 있는 Subscriber들을 대상으로
+      {userId: [1, 2]} 객체를 Publish 하겠다.
+    */
     stompClient.connect({}, function(frame) {
       stompClient.send("/pub/chat/rooms", {}, JSON.stringify({userIds:[1, 2]}));
-      stompClient.disconnect();
+      // stompClient.disconnect();
     });
   };
 
   const enterRoom = (roomId) => {
+    /*
+      Test용 채팅방에서 쓸 user id 입력 - Long 타입으로
+      TODO: 알림 보낸 상대의 user id만 객체로 묶어 보내주기
+    */
     const sender = prompt('대화명을 입력해 주세요.');
     if (sender !== "") {
       localStorage.setItem('wschat.sender', sender);
@@ -55,14 +58,21 @@ function App() {
 
   const connect = () => {
     let reconnect = 0;
+    /*
+      페이지 렌더링 후 실행되는 connect()
+      /chat/rooms 라는 경로를 구독하겠다
+      구독한 채널에서 Publish된 메세지가 왔을 때
+      처리 (recvRoom())
+    */
     stompClient.connect({}, function(frame) {
       stompClient.subscribe("/sub/chat/rooms", function(message) {
-        console.log("오냥")
         const recv = JSON.parse(message.body);
         console.log(recv)
         recvRoom(recv);
       });
     }, function(error) {
+      // 연결이 끊어졌을 때 재연결 시도 부분
+      // 필요할 때 쓰면 될 듯.
       // if(reconnect++ < 5) {
       //   setTimeout(function() {
       //     console.log("connection reconnect");
